0488002b86787e3d0297f4495977d91448275274
Hyun jin <blackduvet52@gmail.com>
2024-08-29 03:41:10 +0900

    카드 레이아웃 및 인터랙티브 요소 추가
    `CarouselDApiDemo` 컴포넌트의 카드 레이아웃 및 인터랙티브 요소를 다음과 같이 업데이트했습니다:
    
    - **카드 레이아웃**:
      - 카드 내용이 포함된 `Card` 컴포넌트를 사용하여 각 프로젝트 정보를 표시.
      - 카드의 레이아웃을 `collet` 상태에 따라 조정하여 보기 모드를 변경할 수 있도록 구현.
    
    - **버튼 및 상태 관리**:
      - '전체보기 / 간추려서보기' 버튼을 추가하여 카드의 디스플레이 모드를 전환할 수 있도록 설정.
      - 버튼 클릭 시 `collet` 상태가 변경되어 카드의 크기와 배치가 동적으로 조정됨.
    
    - **카드 상호작용**:
      - 카드의 이미지에 오버레이 추가: GitHub 및 코드 링크를 포함하여 사용자가 호버할 때 링크가 표시됨.
      - 각 카드에 관련 링크와 아이콘 추가 (React, Vue, GitHub, Figma 등).
      - 카드 상단에 프로젝트 제목 및 설명을 표시하고, 하단에 개발 세부 사항을 나열함.
    
    - **스타일링**:
      - 카드는 `shadow-lg` 클래스를 사용하여 그림자 효과를 추가.
      - 버튼 및 링크에 호버 효과를 추가하여 사용자 인터랙션을 향상시킴.
      - 배지 리스트 및 아이콘 스타일을 조정하여 일관된 시각적 표현을 제공.
    
    이 변경으로 인해 카드 디스플레이 및 사용자 인터랙션이 개선되었으며, 사용자 경험을 보다 직관적으로 제공할 수 있게 되었습니다.

---
+++ src/components/section-porject/carousel-d-api-demo.tsx
@@ -0,0 +1,204 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  BetweenHorizontalEnd,
+  CodeXml,
+  TvMinimal,
+  BetweenHorizontalStart,
+  House,
+} from "lucide-react";
+import Image from "next/image";
+
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { cardContents } from "@/data/card-content";
+import BadgeList from "../ui/badge-list";
+import GithubIcon from "@/assets/icons/github-icon";
+import FigmaIcon from "@/assets/icons/figma-icon";
+import Link from "next/link";
+import ReactIcon from "@/assets/icons/react-icon";
+import VueIcon from "@/assets/icons/vue-icon";
+
+export function CarouselDApiDemo() {
+  const [collet, setCollect] = useState<boolean>(false);
+
+  const handleButtonClickCollte = () => {
+    setCollect(!collet);
+    console.log(collet);
+  };
+
+  const chuka = "Cukapoka 이미지";
+
+  return (
+    <>
+      <div className="flex w-full justify-end">
+        {/* 전체보기 / 간추려서보기 */}
+        <button
+          className="mr-12 border border-foreground/20 hover:border-foreground/40 p-1 rounded-md group"
+          onClick={handleButtonClickCollte}
+        >
+          {collet ? (
+            <BetweenHorizontalStart className="stroke-foreground/50 group-hover:stroke-foreground" />
+          ) : (
+            <BetweenHorizontalEnd className="stroke-foreground/50 group-hover:stroke-foreground" />
+          )}
+        </button>
+      </div>
+      <div className="flex flex-row flex-wrap p-5">
+        {cardContents.map((content, index) => (
+          <div
+            key={index}
+            className={cn("flex p-2", {
+              "w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4": collet,
+              "mx-5": !collet,
+            })}
+          >
+            <Card
+              className={cn("p-2 shadow-lg group flex flex-col", {
+                "w-full ": collet,
+                "w-2/5 mr-4 h-72 md:h-full ": !collet,
+              })}
+            >
+              <CardHeader
+                className={cn("relative", {
+                  "h-40": collet,
+                  "h-60": !collet,
+                })}
+              >
+                <Image
+                  src={content.image.src}
+                  alt={content.image.alt}
+                  className=" rounded-lg w-full h-full"
+                />
+                {/* Hover overlay  inset?*/}
+                <div className="absolute m-1 -top-[2px] bottom-1 inset-0 flex justify-center items-center gap-2 bg-black opacity-0 group-hover:opacity-70 transition-opacity duration-300 rounded-lg ">
+                  {/* github address */}
+                  {content.leftURL.map((item, index) => (
+                    <div className="flex " key={index}>
+                      <Link
+                        href={item.home}
+                        className="mr-1 p-2 border-2 rounded-full border-white/60 hover:border-white"
+                      >
+                        <TvMinimal
+                          className={cn(" stroke-white", {
+                            "w-5 h-5": collet,
+                            "w-5 md:w-7 h-5 md:h-7": !collet,
+                          })}
+                        />
+                      </Link>
+                      <Link
+                        href={item.code}
+                        className="ml-1 p-2 border-2 rounded-full border-white/60 hover:border-white"
+                      >
+                        <CodeXml
+                          className={cn(" stroke-white", {
+                            "w-5 h-5": collet,
+                            "w-5 md:w-7 h-5 md:h-7": !collet,
+                          })}
+                        />
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </CardHeader>
+              <CardContent className="flex flex-col flex-1 justify-between gap-4 relative z-10 ">
+                {/* Title */}
+                <div className="p-1">
+                  <div className="inline-flex">
+                    <Image
+                      src={content.titleIcon.src}
+                      alt={content.titleIcon.alt}
+                      className="w-4 h-4 mt-[2px]"
+                    ></Image>
+                    <span className="text-xs ml-1.5 mb-1 md:text-base">
+                      {content.title}
+                    </span>
+                  </div>
+                  <p className="text-xs md:text-sm text-foreground/80">
+                    {content.subtitle}
+                  </p>
+                </div>
+
+                {/* Stack Badge */}
+                <BadgeList techStack={content.techStack} />
+              </CardContent>
+              <CardFooter className="flex justify-between items-center relative z-10"></CardFooter>
+            </Card>
+            {/* 오른쪽 카드 콘텐츠  */}
+            {!collet && (
+              <div className="flex flex-col">
+                <Card className=" flex flex-col  flex-1  ml-4 shadow-lg">
+                  <CardHeader>
+                    <CardTitle className="text-base md:text-xl">
+                      {content.subtitle}
+                    </CardTitle>
+                    <div className=" text-[11px] md:text-[12px] text-foreground/80 leading-relaxed">
+                      {content.description}
+                    </div>
+                  </CardHeader>
+                  <CardContent className="p-1 text-base md:text-lg leading-1 text-foreground/90">
+                    <h3 className="text-xs md:text-sm font-semibold mb-2 mt-4">
+                      개발내용
+                    </h3>
+                    <ul className="list-disc list-inside  text-[11px] md:text-[12px] mb-4">
+                      {content.details.map((detail) => (
+                        <li key={detail}>{detail}</li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                </Card>
+                <div className="flex justify-around py-5 mt-4 ml-4  rounded-lg shadow-lg">
+                  {content.card.map((item, index) => (
+                    <div
+                      key={index}
+                      className="relative flex flex-col justify-center items-center p-1 rounded-full group"
+                    >
+                      <div className="absolute flex flex-col items-center justify-center transition-all duration-200 ease-in-out">
+                        {item.msg === "react" && (
+                          <Link href={item.src}>
+                            <ReactIcon className="items-center group-hover:fill-blue-400 transition-all duration-200 ease-in-out transform group-hover:-translate-y-1" />
+                          </Link>
+                        )}
+                        {item.msg === "vue" && (
+                          <Link href={item.src}>
+                            <VueIcon className=" group-hover:stroke-blue-400 transition-all duration-200 ease-in-out transform group-hover:-translate-y-1" />
+                          </Link>
+                        )}
+                        {item.msg === "home" && (
+                          <Link href={item.src}>
+                            <House className="group-hover:stroke-blue-400 transition-all duration-200 ease-in-out transform group-hover:-translate-y-1" />
+                          </Link>
+                        )}
+                        {item.msg === "github" && (
+                          <Link href={item.src}>
+                            <GithubIcon className="group-hover:fill-blue-400 transition-all duration-200 ease-in-out transform group-hover:-translate-y-1" />
+                          </Link>
+                        )}
+                        {item.msg === "figma" && (
+                          <Link href={item.src}>
+                            <FigmaIcon className="group-hover:fill-blue-400 transition-all duration-200 ease-in-out transform group-hover:-translate-y-1" />
+                          </Link>
+                        )}
+
+                        <div className="hidden text-xs text-center transform scale-75 opacity-0 translate-y-2 group-hover:block group-hover:scale-100 group-hover:opacity-100 group-hover:translate-y-0 group-hover:text-blue-400 transition-all duration-300 ease-in-out">
+                          {item.msg}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
