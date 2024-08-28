b9c87e12745b82d5b38dc36df8ec2ff338ad9f24
Hyun jin <blackduvet52@gmail.com>
2024-08-29 03:40:42 +0900

    section-about Page Update

---
+++ src/components/section-about/about.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Stacks from "@/components/section-about/stacks";
+import { ArrowBigDownDash } from "lucide-react";
+import { scrollToSection } from "@/lib/utils";
+import Link from "next/link";
+import Introduce from "@/components/section-about/introduce";
+
+export default function About() {
+  return (
+    <section
+      id="about"
+      className="flex flex-col px-10 justify-around min-h-screen scroll-mt-20 "
+    >
+      {/* 제목 */}
+      <div className="flex justify-end border-t-2 border-t-foreground/10">
+        <h1
+          className={`font-bold text-foreground/80 text-5xl tracking-tighter md:text-7xl sm:text-6xl`}
+        >
+          ABOUT.
+          <p className="text-xs tracking-normal text-foreground/50 pl-1 md:text-base sm:text-sm">
+            저는 이런 생각을 가지고 있습니다.
+          </p>
+        </h1>
+      </div>
+      {/* introduce */}
+      <Introduce />
+      {/* stacks */}
+      <Stacks></Stacks>
+      <div className="flex justify-center items-center mt-20 mb-12">
+        {" "}
+        <Link href="#project" onClick={(e) => scrollToSection(e, "#project")}>
+          {" "}
+          <ArrowBigDownDash className="arrow" />
+        </Link>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/section-contact/card.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { Textarea } from "../ui/textarea";
+
+export function CardWithForm() {
+  return (
+    <Card className="flex-1 border-2 border-foreground/10">
+      <CardDescription className="mb-5"></CardDescription>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                placeholder="성함을 입력해 주세요."
+                className=""
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input id="name" placeholder="메일 주소를 입력해주세요." />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="Message">Message</Label>
+              <Textarea placeholder="내용을 입력해 주세요." />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-end mt-10">
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  );
+}
