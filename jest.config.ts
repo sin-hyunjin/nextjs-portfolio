340aac6402178810008f8826dde7ec3bc73a77df
Hyun jin <blackduvet52@gmail.com>
2024-09-03 23:33:42 +0900

    Update : 테스트 코드를 위한 jest 라이브러리 설치  및 셋팅

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,26 @@
-module.exports = {
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
   preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom", // React 컴포넌트 테스트를 위한 설정 "node"
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1", // TypeScript의 paths 설정에 따라 조정
+    "^@/(.*)$": "<rootDir>/src/$1", // 이 설정은 @/ 경로 별칭이 src/ 디렉토리를 참조 하도록함
   },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
+
+export default createJestConfig(config);
