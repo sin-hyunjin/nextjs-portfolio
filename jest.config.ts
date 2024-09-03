7e3b3c3641ff7621e34e51b9516d118376b93277
Hyun jin <blackduvet52@gmail.com>
2024-09-03 22:05:44 +0900

    Create jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", // TypeScript의 paths 설정에 따라 조정
+  },
+};
