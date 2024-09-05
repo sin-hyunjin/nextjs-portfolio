fd971a06123c4ec1d272cbbb2d9c04db1de2e77f
Hyun jin <blackduvet52@gmail.com>
2024-09-05 17:27:02 +0900

    Update error.tsx

--- src/app/(overview)/error.tsx
+++ src/app/(overview)/error.tsx
@@ -1,8 +1,7 @@
-"use client"; //
+"use client";
 
 import { useEffect } from "react";
 
-// 에러 컴포넌트는 클라이언트 컴포넌트여야 함.
 export default function Error({
   error,
   reset,
@@ -15,9 +14,17 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>문제가 발생했습니다!</h2>
-      <button onClick={() => reset()}>다시 시도</button>
+    <div className="flex items-center justify-center min-h-screen  p-4">
+      <div className="max-w-sm w-full bg-white shadow-lg rounded-lg p-6 text-center">
+        <h2 className="text-2xl font-bold  mb-4">문제가 발생했습니다!</h2>
+        <p className="text-gray-600 mb-6">잠시 후 다시 시도해 주세요.</p>
+        <button
+          onClick={() => reset()}
+          className="px-4 py-2 bg-foreground/70 text-white font-semibold rounded-lg hover:bg-foreground focus:outline-none focus:ring-2 focus:ring-blue-300"
+        >
+          다시 시도
+        </button>
+      </div>
     </div>
   );
 }
